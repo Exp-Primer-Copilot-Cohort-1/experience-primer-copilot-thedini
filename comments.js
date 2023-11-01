68210977885e0b32360d291e54ad16f10a98b3cc
Constantine Macris <thedini@users.noreply.github.com>
2023-11-01 03:49:01 +0000

    Copilot thrid committ

--- comments.js
+++ comments.js
@@ -1,1 +1,68 @@
-// Create web server
+// Create web server application
+
+// Import modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const authenticate = require('../authenticate');
+const cors = require('./cors');
+
+// Import models
+const Comments = require('../models/comments');
+
+// Create router
+const commentRouter = express.Router();
+
+// Use body parser to get data from body
+commentRouter.use(bodyParser.json());
+
+// Configure router
+commentRouter.route('/')
+.options(cors.corsWithOptions, (req, res) => {res.sendStatus(200);})
+.get(cors.cors, (req, res, next) => {
+    // Get all comments
+    Comments.find(req.query)
+    .populate('author')
+    .then((comments) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(comments);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    // Create comment
+    Comments.create(req.body)
+    .then((comment) => {
+        console.log('Comment created ', comment);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(comment);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    // Update comment
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /comments');
+})
+.delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    // Delete all comments
+    Comments.deleteMany({})
+    .then((response) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+});
+
+commentRouter.route('/:commentId')
+.options(cors.corsWithOptions, (req, res) => {res.sendStatus(200);})
+.get(cors.cors, (req, res, next) => {
+    // Get comment
+    Comments.findById(req.params.commentId)
+    .populate('author')
+    .then((comment) => {
+        if(comment != null) {
+            res.statusCode = 200
